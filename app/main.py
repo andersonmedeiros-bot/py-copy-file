4af93b8bde8bad3ff8e7025ab96053976a883794
Anderson <anderson.medeiros3011@gmail.com>
2026-07-09 18:55:19 -0300

    feat: implementa funcao de copiar arquivo cp

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    partes = command.split()
+    arquivo_origem = partes[1]
+    arquivo_destino = partes[2]
+
+    if arquivo_origem == arquivo_destino:
+        return
+
+    with open(arquivo_origem, "r") as f1, open(arquivo_destino, "w") as f2:
+        conteudo = f1.read()
+        f2.write(conteudo)
